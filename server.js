d40e77c4578d92bb56dd9ece8bd1390bdf6f774a
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-05 13:51:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/:articleName',function(req,res)
 {
     var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName])); 
+   res.send(createTemplate(article[articleName])); 
 });
 
 app.get('/ui/madi.png', function (req, res) {
