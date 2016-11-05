984abb030d92bd3f427fe8acd6da4c55bc6f9864
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-05 13:39:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ var htmlTemplate=`<html>
 ${title}
 </title>
 <meta name="viewport" content="width-device-width,initial-scale-1" />
+<link rel="ui/style/css" type="text/css">
 </head>
 <body>
    <div class="container">
