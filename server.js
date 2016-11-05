6aa46f4997cda3bab056ac1c46c4075bf442be2d
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-05 13:47:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,15 +64,10 @@ ${title}
 `;
 return htmlTemplate;
 }
-
-
-
-
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function(req,res)
+app.get('/:articleName',function(req,res)
 {
    res.send(createTemplate(articleOne)); 
 });
