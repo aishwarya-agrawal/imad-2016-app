bc8383b812c92ef1a8eabcecb55426e65848c71d
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-11 23:53:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('registration.html',function(req,res)
+app.get('/ui/registration.html',function(req,res)
 {
     res.sendFile(path.join(__dirname,'ui','registration.html'));
 })
