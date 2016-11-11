73e19f9a334d5983b4d1c99dfaec73394a168c7a
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-11 11:48:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,69 +16,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var article={
-'article-one':{
-    title: 'Article one | Aishwarya Agrawal',
-    heading : 'Article-one',
-    date : 'November 5,2016',
-    content:`
-    <p> This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first            Article.This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first        Article.This is my first Article.<p>`
-    
-},
-'article-two':{title: 'Article two | Aishwarya Agrawal',
-    heading : 'Article-two',
-    date : 'November 5,2016',
-    content:`
-    <p> This is my second Article.This is my first Article.<p>`
-    },
-'article-three':{title: 'Article three | Aishwarya Agrawal',
-    heading : 'Article-three',
-    date : 'November 5,2016',
-    content:`
-    <p> This is my third Article.This is my first Article.<p>`
-    }
-};
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-var htmlTemplate=`<html>
-<head>
-<title>
-${title}
-</title>
-<meta name="viewport" content="width-device-width,initial-scale-1" />
-<link rel="stylesheet" type="text/css" href="ui/style.css">
-</head>
-<body>
-   <div class="container">
-      <div>
-      <a href="/">Home</a>
-      </div>
-      <hr/>
-      <h3>
-      ${heading}
-      </h3>
-      <div>
-      ${date}
-      </div>
-      <div>
-      ${content}
-      </div>
-      </div>
-</body>
-</html>
-`;
-return htmlTemplate;
-}
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/:articleName',function(req,res)
-{
-    var articleName=req.params.articleName;
-   res.send(createTemplate(article[articleName])); 
 });
 
 app.get('/ui/madi.png', function (req, res) {
