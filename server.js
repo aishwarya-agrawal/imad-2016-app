d50884f3fa0a1578a48d80c13bf0c85d58425ac6
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-11 23:37:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
-});https://cloud.imad.hasura.io/code/files/ui%2Fmain.js
+});
