b7419451b75acd51e5f48e3f87b22207b3b72690
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-12 20:44:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.get('/ui/write.html',function(req,res)
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/pencil-1486278.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pencil-1486278.jpg'));
+});
+app.get('/ui/typewriter-801921.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'typewriter-801921.jpg'));
+});
+app.get('/ui/clipboard-1772235_960_720.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'clipboard-1772235_960_720.png'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
