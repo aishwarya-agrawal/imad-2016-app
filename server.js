cab18255d3e0220528f5879727910ab74365f8e3
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-12 21:07:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,24 +12,6 @@ var config={
 }
 var app = express();
 app.use(morgan('combined'));
-var pool=new Pool(config);
-app.get('test-db',function(req,res)
-{
-	pool.query("SELECT * FROM test",function(err,result)
-	{
-		if(err)
-		{
-			res.status(500).send(err.toString());
-		}
-		else
-		{
-			res.send(JSON.stringify(result.rows));
-		}
-	});
-});
-
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -55,6 +37,22 @@ app.get('/ui/typewriter-801921.jpg', function (req, res) {
 app.get('/ui/clipboard-1772235_960_720.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'clipboard-1772235_960_720.png'));
 });
+var pool=new Pool(config);
+app.get('test-db',function(req,res)
+{
+	pool.query("SELECT * FROM test",function(err,result)
+	{
+		if(err)
+		{
+			res.status(500).send(err.toString());
+		}
+		else
+		{
+			res.send(JSON.stringify(result.rows));
+		}
+	});
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
