669e7bab0d27f2b5794a9c5be80b9277193d2118
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-12 09:14:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').Pool;
 
 var app = express();
 app.use(morgan('combined'));
@@ -20,11 +21,6 @@ app.get('/ui/write.html',function(req,res)
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
