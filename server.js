25d51e692b7e471a919dfecef46652acc1214f41
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-12 21:13:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path=require('path');
-var pool=require('pg').Pool;
-var config={
-	user:'aishwarya-agrawal',
-	database:'aishwarya-agrawal',
-	host:'db.imad.hasura-app.io',
-	port:'5432',
-	password:process.env.DB_PASSWORD
-
-}
 var app = express();
 app.use(morgan('combined'));
 app.get('/', function (req, res) {
@@ -37,22 +28,6 @@ app.get('/ui/typewriter-801921.jpg', function (req, res) {
 app.get('/ui/clipboard-1772235_960_720.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'clipboard-1772235_960_720.png'));
 });
-var pool=new Pool(config);
-app.get('test-db',function(req,res)
-{
-	pool.query("SELECT * FROM test",function(err,result)
-	{
-		if(err)
-		{
-			res.status(500).send(err.toString());
-		}
-		else
-		{
-			res.send(JSON.stringify(result.rows));
-		}
-	});
-});
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
