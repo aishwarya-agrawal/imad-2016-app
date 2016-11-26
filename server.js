bcb2d41fbaf20c0d8830ab3a78cd39c4ef410c0b
aishwarya-agrawal <ashley.agrawal265@gmail.com>
2016-11-26 10:19:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,24 +11,6 @@ var config={
 };
 var pool = new pg.Pool(config);
 
-// to run a query we can acquire a client from the pool,
-// run a query on the client, and then return the client to the pool
-pool.connect(function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
-
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].number);
-    //output: 1
-  });
-});
-
 
 var app = express();
 app.use(morgan('combined'));
